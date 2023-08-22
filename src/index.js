0628d97dfc76a2d6361bcc8d61916ec217467e69
vlorian <vlorian.dervishi@gmail.com>
2023-08-22 13:11:03 -0400

    render images of dogs on the page

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 console.log("%c HI", "color: firebrick");
 const breedUrl = "https://dog.ceo/api/breeds/list/all";
-
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
 document.addEventListener("DOMContentLoaded", async () => {
   selectDropElement = document.querySelector("#breed-dropdown");
   displayBreed = document.querySelector("#dog-breeds");
+  imgcontainer = document.querySelector("#dog-image-container");
   async function dogFetcher() {
     const response = await fetch(breedUrl);
     const data = await response.json();
-    getBreed(data.message);
+    const imgresponse = await fetch(imgUrl);
+    const imgData = await imgresponse.json();
+    console.log(imgData.message);
+    getBreed(data.message, imgData.message);
   }
 
-  function getBreed(breed) {
+  function getBreed(breed, imgData) {
+    Object.values(imgData).forEach((render, index) => {
+      const imgEl = document.createElement("img");
+      const divEl = document.createElement("div");
+      imgEl.src = render;
+      divEl.append(imgEl);
+      imgcontainer.append(divEl);
+    });
     selectDropElement.addEventListener("change", (e) => {
       e.preventDefault();
       console.log(selectDropElement.value);
@@ -21,11 +32,9 @@ document.addEventListener("DOMContentLoaded", async () => {
       });
     });
   }
-  function showcaseBreed(pet) {
-    ulElement = document.createElement("ul");
+  function showcaseBreed(pet, image) {
     liElement = document.createElement("li");
     liElement.textContent = pet;
-    ulElement.append(liElement);
     displayBreed.prepend(liElement);
   }
 
